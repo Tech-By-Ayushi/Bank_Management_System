eabc169b7f57ec02443a15f698a24176602f0d9f
Ayushi Sharma <ayushisharma27823@gmail.com>
2025-02-09 17:48:24 +0530

    Add files via upload

---
+++ conn.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+
+
+public class conn {
+    Connection c;
+    Statement s;
+    public conn(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c= DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "username","Password");
+            s=c.createStatement();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
